a905064e51e8bcf551c1faf1c0c63a259abe9a9a
athdromeda <anixatho01@gmail.com>
2024-04-29 12:28:30 +0700

    fix: move CountryDetails type to /types

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -3,20 +3,6 @@ import { Link, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 
-type CountryDetails = {
-  __typename: "Country";
-  awsRegion: string;
-  capital: string;
-  code: string;
-  currencies: string[];
-  currency: string;
-  emoji: string;
-  name: string;
-  native: string;
-  phone: string;
-  phones: string[];
-};
-
 const Country = () => {
   const { code } = useParams();
 
---
+++ src/types/CountryDetails.ts
@@ -0,0 +1,15 @@
+type CountryDetails = {
+  __typename: "Country";
+  awsRegion: string;
+  capital: string;
+  code: string;
+  currencies: string[];
+  currency: string;
+  emoji: string;
+  name: string;
+  native: string;
+  phone: string;
+  phones: string[];
+};
+
+export default CountryDetails;
